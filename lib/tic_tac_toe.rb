ceaf523bc4ca86034089b0b980d24ca2aed04c5f
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-17 06:50:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,6 +66,10 @@ class TicTacToe
       if @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
         return win_combo
     elsif @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
+    return win_combo
+    end
+      false
+  end
   end
   
   def full?
