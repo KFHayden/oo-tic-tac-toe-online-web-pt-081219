f89261eddf5a69c836f383cc3059561ea7bfe426
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-17 06:46:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,10 @@ class TicTacToe
   end
   
   def won?
-    WIN_COMBINATIONS.each do |
+    WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
   end
   
 end
