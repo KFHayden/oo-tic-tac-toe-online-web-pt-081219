29069172305077de1922a6870d9045adbce765bf
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-17 06:54:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,6 +69,7 @@ class TicTacToe
         @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
         return win_combo
       end
+      false
     end
   end
   
